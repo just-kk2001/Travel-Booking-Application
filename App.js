8d698be8164b70e57c2fb5b86efe4cf4b0c5a53d
just-kk2001 <kaustubhkumbhar2468@gmail.com>
2024-10-10 17:47:45 +0530

    Add files via upload

--- App.js
+++ App.js
@@ -1,3 +1,74 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import "./App.css";
+
+function App() {
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const [userInfo, setUserInfo] = useState();
+  
+  const onSubmit = (data) => {
+    setUserInfo(data);
+    console.log(data);
+  };
+
+  return (
+    <div className='container'>
+      <pre>{JSON.stringify(userInfo, undefined, 2)}</pre>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <h1>Registration Form</h1>
+        <div className='ui divider'></div>
+        <div className='ui form'>
+          <div className='fields'>
+            <label>Username</label>
+            <input 
+              type='text' 
+              {...register("username", { required: "Username is required" })} 
+              placeholder='Username' 
+            />
+            <p>{errors.username?.message}</p>
+          </div>
+          
+          <div className='fields'>
+            <label>Email</label>
+            <input 
+              type='email' 
+              {...register("email", { 
+                required: "Email is required", 
+                pattern: { value: /^\S+@\S+$/i, message: "This is not a valid email" } 
+              })} 
+              placeholder='Email' 
+            />
+            <p>{errors.email?.message}</p>
+          </div>
+          
+          <div className='fields'>
+            <label>Password</label>
+            <input 
+              type='password' 
+              {...register("password", { 
+                required: "Password is required",
+                minLength: { value: 4, message: "Password must be more than 4 characters" },
+                maxLength: { value: 10, message: "Password cannot exceed more than 10 characters" }
+              })} 
+              placeholder='Password' 
+            />
+            <p>{errors.password?.message}</p>
+          </div>
+          
+          <button className='fluid ui button blue'>Submit</button>
+        </div>
+      </form>
+    </div>
+  );
+}
+
+export default App;
+
+
+
+
+
+
 /*import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './component/hello';
@@ -25,7 +96,7 @@ export default App;
 
 
 
-
+/*
 //Preloader Ethun chalu
 import React, { useState } from 'react';
 import SplashScreen from './component/SplashScreen';
@@ -64,3 +135,4 @@ export default App
 //   )
 // }
 // export default App
+*/
